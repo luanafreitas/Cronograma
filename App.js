069ddf0b92bc0f76c6f20dbde449e5d87270a876
luanafreitas <luanaluciele0@gmail.com>
2022-04-15 16:45:48 -0300

    Commit inicial

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello</Text>
+      <Text>Hi</Text>
       <StatusBar style="auto" />
     </View>
   );
